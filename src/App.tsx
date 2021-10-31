a924fa81c0e8d76fed1d0d45c4845f9beedd25a5
ml250271 <lukicmilica@hotmail.com>
2021-10-31 21:24:24 +0100

    add form for new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AddTodo } from "./components/AddTodo";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -6,10 +7,25 @@ import Header from "./components/Header";
 function App() {
 const [todos, setTodos] = useState<Todo[]>([])
 
+// const toggleComplete: ToggleComplete = selectedTodo => {
+//   const updatedTodos = todos.map(todo => {
+//     if (todo === selectedTodo) {
+//       return { ...todo, complete: !todo.complete };
+//     }
+//     return todo;
+//   });
+//   setTodos(updatedTodos);
+// };
+
+const addTodo: AddTodo = newTodo => {
+  newTodo.trim() !== "" &&
+    setTodos([...todos, { text: newTodo, complete: false }]);
+};
+
   return (
     <>
       <Header />
-      <p>Kanban Board</p>
+      <AddTodo addTodo={addTodo} />
       <Footer />
     </>
   );
